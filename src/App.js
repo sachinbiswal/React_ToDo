7f907d08fd8fc2b824d7609a804585c74f756456
sachin kumar biswal <sachinkbiswal@gmail.com>
2023-04-22 23:12:52 +0530

    React ToDo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Addtodo from './Component/Addtodo';
+
+function App() {
+  return (
+    <div className="App">
+      <Addtodo />
+    </div>
+  );
+}
+
+export default App;
